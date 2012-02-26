447a503e52b14d402810e29e06f80a1619209619
striglia <scott.triglia@gmail.com>
2012-02-26 15:09:22 -0800

    Added spacing to cards classes

--- cards.py
+++ cards.py
@@ -45,6 +45,7 @@ class Province(Card):
     @staticmethod
     def vp(deck):
         return 6
+
 class Duchy(Card):
     name = 'Duchy'
     price = 5
@@ -53,6 +54,7 @@ class Duchy(Card):
     @staticmethod
     def vp(deck):
         return 3
+
 class Estate(Card):
     name = 'Estate'
     price = 2
@@ -61,6 +63,7 @@ class Estate(Card):
     @staticmethod
     def vp(deck):
         return 1
+
 class Curse(Card):
     name = 'Curse'
     price = 0
@@ -69,16 +72,19 @@ class Curse(Card):
     @staticmethod
     def vp(deck):
         return -1
+
 class Gold(Card):
     name = 'Gold'
     price = 6
     plus_coins = 3
     is_implemented = True
+
 class Silver(Card):
     name = 'Silver'
     price = 3
     plus_coins = 2
     is_implemented = True
+
 class Copper(Card):
     name = 'Copper'
     price = 0
@@ -92,18 +98,21 @@ class Adventurer(Card):
     is_action = True
     # No idea how to do conditional drawing yet =(
     is_implemented = False
+
 class Bureaucrat(Card):
     name = 'Bureaucrat'
     price = 4
     is_action = True
     # No idea how to do so much here
     is_implemented = False
+
 class Chancellor(Card):
     name = 'Chancellor'
     price = 3
     is_action = True
     plus_coins = 2
     is_implemented = False
+
 class Cellar(Card):
     name = 'Cellar'
     price = 2
@@ -111,69 +120,90 @@ class Cellar(Card):
     plus_actions = 1
     # No idea how to do conditional discarding yet =(
     is_implemented = False
+
 class Chapel(Card):
     name = 'Chapel'
     price = 2
     is_action = True
     # Conditional discard
     is_implemented = False
+
 class CouncilRoom(Card):
     name = 'Council Room'
     is_implemented = False
+
 class Feast(Card):
     name = 'Feast'
     is_implemented = False
+
 class Festival(Card):
     name = 'Festival'
     is_implemented = False
+
 class Gardens(Card):
     name = 'Gardens'
     is_implemented = False
+
 class Laboratory(Card):
     name = 'Laboratory'
     is_implemented = False
+
 class Library(Card):
     name = 'Library'
     is_implemented = False
+
 class Market(Card):
     name = 'Market'
     is_implemented = False
+
 class Militia(Card):
     name = 'Militia'
     is_implemented = False
+
 class Mine(Card):
     name = 'Mine'
     is_implemented = False
+
 class Moat(Card):
     name = 'Moat'
     is_implemented = False
+
 class Moneylender(Card):
     name = 'Moneylender'
     is_implemented = False
+
 class Remodel(Card):
     name = 'Remodel'
     is_implemented = False
+
 class Smithy(Card):
     name = 'Smithy'
     is_implemented = False
+
 class Spy(Card):
     name = 'Spy'
     is_implemented = False
+
 class Thief(Card):
     name = 'Thief'
     is_implemented = False
+
 class ThroneRoom(Card):
     name = 'ThroneRoom'
     is_implemented = False
+
 class Village(Card):
     name = 'Village'
     is_implemented = False
+
 class Witch(Card):
     name = 'Witch'
     is_implemented = False
+
 class Woodcutter(Card):
     name = 'Woodcutter'
     is_implemented = False
+
 class Workshop(Card):
     name = 'Workshop'
     is_implemented = False
